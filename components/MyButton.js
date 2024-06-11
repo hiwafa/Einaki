7991081bbd614fc4975752eeabbe1882c4973d37
hiwafa <mq.func@gmail.com>
2024-06-11 16:53:20 +0200

    icon added to button

--- components/MyButton.js
+++ components/MyButton.js
@@ -1,10 +1,16 @@
 import { StyleSheet, Text, Pressable } from 'react-native';
+import FontAwesome from "@expo/vector-icons/FontAwesome";
 
 export default function MyButton({ label }) {
     return (
         <view style={styles.container}>
-
             <Pressable style={styles.buttonStyle} onPress={() => { alert('Hi dear') }}>
+                <FontAwesome
+                    name="picture-o"
+                    size={18}
+                    color="#fff"
+                    style={styles.buttonIcon}
+                />
                 <Text style={styles.buttonLabel}>{label}</Text>
             </Pressable>
         </view>
@@ -26,10 +32,14 @@ const styles = StyleSheet.create({
         height: '100%',
         alignItems: 'center',
         justifyContent: 'center',
-        flexDirection: 'row'
+        flexDirection: 'row',
+        backgroundColor: 'orange'
     },
     buttonLabel: {
         color: '#fff',
         fontSize: 16,
+    },
+    buttonIcon: {
+        paddingRight: 8,
     }
 });
