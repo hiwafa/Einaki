14032a3b7d79e71f6428d6f37474b2680ee7743a
hiwafa <mq.func@gmail.com>
2024-06-12 23:42:03 +0200

    second routing in expo

--- app/about.tsx
+++ app/about.tsx
@@ -1,10 +1,15 @@
-import { View, Text  } from "react-native";
+import { useRouter } from "expo-router";
+import { View, Text, Pressable  } from "react-native";
 
 
 export default function AboutScreen(){
+    const router = useRouter();
     return (
         <View>
             <Text>About Page</Text>
+            <Pressable onPress={()=> router.back()}>
+                <Text>Go Back</Text>
+            </Pressable>
         </View>
     )
 }
